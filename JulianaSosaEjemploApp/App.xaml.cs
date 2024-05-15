736d868d76f2b3c223bc6b21c3fbe9901198d598
cisco <cisco@UPOAULA10306>
2024-05-15 11:13:15 -0500

    Agregar archivos de proyecto.

---
+++ JulianaSosaEjemploApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JulianaSosaEjemploApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent(); //mayoria de errores en este metodo
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JulianaSosaEjemploApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JulianaSosaEjemploApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
