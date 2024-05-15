f78140db746c8f28288372ebb928b55d7309d243
JulianaSosa99 <juliana.sosa@udla.edu.ec>
2024-05-15 12:23:39 -0500

    CAMBIOS REALIZADOS SI FUNCIONA

--- JulianaSosaEjemploApp/MainPage.xaml.cs
+++ JulianaSosaEjemploApp/MainPage.xaml.cs
@@ -9,24 +9,14 @@
             InitializeComponent();//realiza la interpretacion a cs
         }
 
-        private void OnCounterClicked(object sender, EventArgs e) //de los conceptos mas importantes "EVENT HANDLER: le decimos a la app que quiere que haag cuando sucede un evento hola"
-        {
-            count++;
-
-          //  if (count == 1)
-              //  CounterBtn.Text = $"Clicked {count} time";
-          //  else
-             //   CounterBtn.Text = $"Clicked {count} times";
-
-            //SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        
 
         //creacion del metodo ontraslate tras poner click derecho
         private void OnTranslate(object sender, EventArgs e)
         {
            
             string enteredNumber = PhoneNumberText.Text;
-          //  translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+          translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
 
             if (!string.IsNullOrEmpty(translatedNumber))
             {
