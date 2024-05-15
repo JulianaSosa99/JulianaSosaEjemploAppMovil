e5dade6f77dec9b39332c31019feea1ce792f5d4
JulianaSosa99 <juliana.sosa@udla.edu.ec>
2024-05-15 12:10:16 -0500

    Definir botores, etiquetas u comentarios en Main Page XAML

--- JulianaSosaEjemploApp/MainPage.xaml.cs
+++ JulianaSosaEjemploApp/MainPage.xaml.cs
@@ -3,7 +3,7 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
+        string translatedNumber;
         public MainPage()
         {
             InitializeComponent();//realiza la interpretacion a cs
@@ -13,12 +13,44 @@
         {
             count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+          //  if (count == 1)
+              //  CounterBtn.Text = $"Clicked {count} time";
+          //  else
+             //   CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        //creacion del metodo ontraslate tras poner click derecho
+        private void OnTranslate(object sender, EventArgs e)
+        {
+           
+            string enteredNumber = PhoneNumberText.Text;
+          //  translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        //convertimos el metodo a asincronico
+         async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+        "Dial a Number",
+        "Would you like to call " + translatedNumber + "?",
+        "Yes",
+        "No"))
+            {
+                // TODO: dial the phone
+            }
         }
     }
 
